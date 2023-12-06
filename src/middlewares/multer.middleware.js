4e919fe3d1e19f6925ce40eec71ea34b8527b64f
Rupesh Jadhav <rupesh.jadhav7700@gmail.com>
2023-12-06 19:12:30 +0530

    File uploading and multer

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,14 @@
+import multer from "multer";
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "./public/temp");
+    },
+    filename: function (req, file, cb) {
+        //same filename can be saved so change for FUTURE
+        cb(null, file.originalname);
+        console.log(file);
+    },
+});
+
+export const upload = multer({ storage });
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,31 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if (!localFilePath) return null;
+
+        //uploading file on cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto",
+        });
+
+        console.log("File has been uploaded successfully", response.url);
+        return response;
+
+    } catch (error) {
+        //this is for removing temp files from local saved as the upload operation got failed.
+
+        fs.unlinkSync(localFilePath)
+        return null
+
+    }
+};
+
+export {uploadOnCloudinary}
